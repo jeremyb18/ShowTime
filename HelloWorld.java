f7ec15103773302dc22605b53fd8978d3841c90d
Jeremy Ruebush <jeremy.ruebush@tooeleschools.org>
2019-08-28 09:37:49 -0600

    HelloWorld

---
+++ HelloWorld.java
@@ -0,0 +1,22 @@
+//Jeremy
+//8/28/19
+//Hello World
+
+
+
+public class HelloWorld
+{
+   
+   public static void main(String[] args)
+   {
+      //Lets create a whole new world
+/* 
+lots
+    of
+      stuff
+          */
+      	System.out.println("Hello World");
+	
+   }
+}
+
