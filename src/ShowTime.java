7c2882720ed61dff25d274e841cd8b6d455feb82
Jeremy Ruebush <jeremy.ruebush@tooeleschools.org>
2019-08-30 09:37:28 -0600

    ShowTime

---
+++ src/ShowTime.java
@@ -0,0 +1,21 @@
+//package Jeremy.CLC.ShowTime;
+
+import java.util.Calendar;
+
+
+
+class ShowTime
+{
+
+	public static void main(String[] args)
+	{
+	System.out.println(Calendar.getInstance().getTime());
+	
+	
+	}
+
+
+
+
+
+}
